5f387fc30fdf72e6f080baec004b0bc816c61b63
maxsegeda <132485806+maxsegeda@users.noreply.github.com>
2024-04-12 17:11:51 +0200

    test

---
+++ client/src/plugins/apollo.js
@@ -0,0 +1,20 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+export const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
